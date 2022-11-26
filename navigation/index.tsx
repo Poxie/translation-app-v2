32b1239851785ea9686128d68c10d299fe06ac56
= <poxen12@hotmail.com>
2022-11-26 02:39:04 +0100

    Added typed navigation

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,9 +1,25 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from '@react-navigation/native';
 import Home from "../components/home/Home";
+import { SelectItems } from "../components/select/SelectItems";
+import { MainStackParamList, ModalStackParamList, RootStackParamList } from "../types";
 
-const MainStack = createNativeStackNavigator();
+// Root stack
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+const RootStackProvider = () => {
+    return(
+        <RootStack.Navigator 
+            initialRouteName={'Root'}
+            screenOptions={{ headerShown: false }}
+        >
+            <RootStack.Screen name="Root" component={MainStackProvider} />
+            <RootStack.Screen name="Modal" component={ModalStackProvider} options={{ presentation: 'modal' }} />
+        </RootStack.Navigator>
+    )
+}
 
+// Main stack
+const MainStack = createNativeStackNavigator<MainStackParamList>();
 const MainStackProvider = () => {
     return(
         <MainStack.Navigator initialRouteName={'Home'}>
@@ -12,11 +28,21 @@ const MainStackProvider = () => {
     )
 }
 
+// Modal stack
+const ModalStack = createNativeStackNavigator<ModalStackParamList>();
+const ModalStackProvider = () => {
+    return(
+        <ModalStack.Navigator>
+            <ModalStack.Screen name="Select Items" component={SelectItems} />
+        </ModalStack.Navigator>
+    )
+}
+
 // Exporting navigation
 export default function Navigation() {
     return(
         <NavigationContainer>
-            <MainStackProvider />
+            <RootStackProvider />
         </NavigationContainer>
     )
 }
---
+++ types.ts
@@ -0,0 +1,30 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NavigatorScreenParams } from '@react-navigation/native';
+import { SelectItem } from "./components/select";
+
+// Overriding useNavigation autocorrection
+declare global {
+    namespace ReactNavigation {
+        interface RootParamList extends RootStackParamList {}
+    }
+}
+
+// Stack props
+export type RootStackParamList = {
+    'Root': NavigatorScreenParams<MainStackParamList>;
+    'Modal': NavigatorScreenParams<ModalStackParamList>;
+}
+export type MainStackParamList = {
+    'Home': undefined;
+}
+
+export type ModalStackParamList = {
+    'Select Items': {
+        items: SelectItem[];
+        active: SelectItem;
+        onChange: (id: string) => void;
+    }
+}
+
+// Screen props
+export type SelectItemScreenProps = NativeStackScreenProps<ModalStackParamList, 'Select Items'>
