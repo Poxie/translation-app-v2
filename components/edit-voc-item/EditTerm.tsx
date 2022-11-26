814b8b4436c5f26dbffdba3fe57a5a0064e671c4
= <poxen12@hotmail.com>
2022-11-26 07:18:32 +0100

    Added ability to choose category for item

--- components/edit-voc-item/EditTerm.tsx
+++ components/edit-voc-item/EditTerm.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { View } from 'react-native';
 import layout from '../../constants/layout';
-import { useAppDispatch } from '../../redux/store';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { addTerm } from '../../redux/voc/actions';
+import { selectCategories } from '../../redux/voc/selectors';
 import { VocItem } from '../../types';
 import Button from '../button';
 import Input from '../input';
+import Select from '../select';
 
 export const EditTerm: React.FC<{
     defaultItem?: VocItem;
@@ -14,6 +16,7 @@ export const EditTerm: React.FC<{
     const [term, setTerm] = useState(defaultItem?.term || '');
     const [definition, setDefinition] = useState(defaultItem?.definition || '');
     const [parentId, setParentId] = useState(defaultItem?.parentId || null);
+    const availableParents = useAppSelector(selectCategories);
     const disabled = !term && !definition;
 
     const createTerm = () => {
@@ -33,6 +36,10 @@ export const EditTerm: React.FC<{
         dispatch(addTerm(termItem));
     }
 
+    const parentItems = availableParents.map(category => ({
+        id: category.id,
+        text: category.title as string
+    }))
     return(
         <View style={styles.container}>
             <View>
@@ -47,6 +54,13 @@ export const EditTerm: React.FC<{
                     label={'Definition'}
                     onTextChange={setDefinition}
                     containerStyle={styles.inputContainer}
+                />
+                <Select 
+                    onChange={ids => setParentId(ids[0])}
+                    containerStyle={styles.inputContainer}
+                    selectableItems={parentItems}
+                    header={'Choose category'}
+                    label={'Category'}
                 />
             </View>
             <Button 
--- redux/voc/selectors.ts
+++ redux/voc/selectors.ts
@@ -1,8 +1,8 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "../store"
 
-const selectTerms = (state: RootState) => state.voc.terms;
-const selectCategories = (state: RootState) => state.voc.categories;
+export const selectTerms = (state: RootState) => state.voc.terms;
+export const selectCategories = (state: RootState) => state.voc.categories;
 const selectFloatingTerms = (state: RootState) => state.voc.terms.filter(term => !term.parentId);
 const selectFloatingCategories = (state: RootState) => state.voc.categories.filter(category => !category.parentId);
 const selectId = (_:any, id: string) => id;
