856fcc8ce295d8068eddcc7197e896c30ea1d51a
= <poxen12@hotmail.com>
2022-11-30 00:05:40 +0100

    Added some UI for result items

--- components/quiz/QuizResultItem.tsx
+++ components/quiz/QuizResultItem.tsx
@@ -1,20 +1,52 @@
+import { Feather } from '@expo/vector-icons';
 import { View } from "react-native";
 import { PlayedTerm } from ".";
+import layout from "../../constants/layout";
+import { useColors } from '../../hooks/useColors';
 import { useAppSelector } from "../../redux/store";
 import { selectTermById } from "../../redux/voc/selectors";
 import Text from "../text";
 
 export const QuizResultItem: React.FC<{
     id: string;
+    isLast: boolean;
     outcome: PlayedTerm['outcome'];
-}> = ({ id, outcome }) => {
+}> = ({ id, isLast, outcome }) => {
+    const { color: { red, green } } = useColors();
     const term = useAppSelector(state => selectTermById(state, id));
 
     return(
-        <View>
-            <Text>
-                {term?.term || ''}: {outcome}
-            </Text>
+        <View style={{
+            marginBottom: !isLast ? layout.spacing.primary : 0,
+            ...styles.container
+        }}>
+            <View>
+                <Text style={styles.term}>
+                    {term?.term || ''}
+                </Text>
+                <Text>
+                    {term?.definition || ''}
+                </Text>
+            </View>
+
+            {outcome === 'correct' && (
+                <Feather name="check-circle" size={24} color={green} />
+            )}
+            {outcome === 'incorrect' && (
+                <Feather name="x-circle" size={24} color={red} />
+            )}
         </View>
     )
+}
+const styles = {
+    container: {
+        flexDirection: 'row' as 'row',
+        justifyContent: 'space-between' as 'space-between',
+        alignItems: 'center' as 'center'
+    },
+    term: {
+        fontSize: 16,
+        fontWeight: '600' as '600',
+        marginBottom: 4
+    }
 }
--- components/quiz/QuizResultsScreen.tsx
+++ components/quiz/QuizResultsScreen.tsx
@@ -1,24 +1,50 @@
-import { View } from "react-native"
+import { ScrollView, View } from "react-native"
 import { PlayedTerm } from "."
+import layout from "../../constants/layout";
+import { useColors } from "../../hooks/useColors";
 import Text from "../text"
 import { QuizResultItem } from "./QuizResultItem";
 
 export const QuizResultsScreen: React.FC<{
     results: PlayedTerm[];
 }> = ({ results }) => {
+    const { background: { secondary, tertiary }, text: { secondary: textSecondary } } = useColors();
+
     return(
-        <View>
-            <Text>
+        <ScrollView style={styles.container}>
+            <Text style={{
+                color: textSecondary,
+                ...styles.header
+            }}>
                 Results
             </Text>
-            <View>
-                {results.map(result => (
+            <View style={{
+                backgroundColor: secondary,
+                borderColor: tertiary,
+                ...styles.resultContainer
+            }}>
+                {results.map((result, key) => (
                     <QuizResultItem 
                         {...result}
+                        isLast={key === results.length -  1}
                         key={result.id}
                     />
                 ))}
             </View>
-        </View>
+        </ScrollView>
     )
+}
+const styles = {
+    container: {
+        padding: layout.spacing.primary
+    },
+    header: {
+        fontWeight: '600' as '600',
+        marginBottom: layout.spacing.secondary
+    },
+    resultContainer: {
+        padding: layout.spacing.secondary,
+        borderRadius: layout.borderRadius.secondary,
+        borderWidth: layout.borderWidth.secondary
+    }
 }
